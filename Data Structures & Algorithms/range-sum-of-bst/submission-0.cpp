a3907cdb9a96fcd3ee91b02d2153a1b06d793dff
Muhammad Awais Khalil <muhammadawaiskhalil3@gmail.com>
2026-08-24 20:24:01 +0500

    Add: range-sum-of-bst - submission-0

---
+++ Data Structures & Algorithms/range-sum-of-bst/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==nullptr){
+            return 0;
+        }
+
+        // if(root->val>=low&&root->val<=high){
+        //     return root->val;
+        // }
+        
+        int leftSum=rangeSumBST(root->left,low,high);
+        int rightSum=rangeSumBST(root->right,low,high);
+        if(root->val>=low&&root->val<=high){
+            return root->val+leftSum+rightSum;
+        }else{
+            return leftSum+rightSum;
+        }
+        
+    }
+};
